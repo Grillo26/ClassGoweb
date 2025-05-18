f43be7ad250e75252460eff25dfe3e9e877418a7
alejandoCalzadilla <calzadillanogalesalejandro@gmail.com.com>
2025-05-18 14:24:45 -0400

    agregar materias de tutor sin imagenes

--- resources/views/livewire/pages/tutor/manage-sessions/manage-subjects.blade.php
+++ resources/views/livewire/pages/tutor/manage-sessions/manage-subjects.blade.php
@@ -43,6 +43,7 @@
             </button>
         </div>
 
+        <!-- Contenido -->
         @if($isLoading)
             @include('skeletons.manage-subject')
         @else
@@ -186,12 +187,13 @@
             </div>
         @endif
 
-        <!-- Modals -->
+        <!-- Modals crear o editar -->
         <div wire:ignore.self class="modal am-modal fade am-subject_modal" id="subject_modal" data-bs-backdrop="static">
             <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-
-
+                
+                
+                 <div class="modal-content">
+                       <!-- Modal  Header-->
                     <div class="am-modal-header">
                         <template x-if="sessionData.edit_id">
                             <h2>{{ __('subject.edit_subject') }} </h2>
@@ -205,9 +207,10 @@
                     </div>
 
 
-
+                     <!-- Modal formulario -->
                     <div class="am-modal-body">
-                        <form class="am-themeform am-modal-form">
+                       
+                    <form class="am-themeform am-modal-form">
                             <fieldset>
                                 <div @class(['form-group', 'am-invalid'=> $errors->has('form.subject_id')])>
                                     <label class="am-label am-important2" for="subjects">
@@ -235,7 +238,7 @@
                                        
                                   <x-input-label class="am-important2" for="introduction" :value="__('subject.breif_introduction')" />
                                     <div class="am-custom-editor" wire:ignore>
-                                        <textarea id="subject_desc" class="form-control" placeholder="{{ __('subject.add_introduction') }}">{{ $form->description }}</textarea>
+                                        <textarea id="subject_desc" class="form-control" placeholder="{{ __('subject.add_introduction') }}" wire:model.defer="form.description"></textarea>
                                         <span class="characters-count"></span>
                                     </div>
                                     <x-input-error field_name="form.description" />
@@ -243,10 +246,11 @@
 
 
 
-
+                                 {{--  
                                 <div class="form-group">
                                     <x-input-label for="Profile1" :value="__('general.upload_image')" />
                                     <div class="am-uploadoption" x-data="{isUploading:false}" wire:key="uploading-profile-{{ time() }}">
+
 
 
                                         <div class="tk-draganddrop"
@@ -303,16 +307,26 @@
                                         <x-input-error field_name="form.image" />
                                     </div>
                                 </div>
+                                --}}
 
                                 <div class="form-group am-mt-10 am-form-btn-wrap">
                                     <button class="am-btn" wire:click.prevent="saveNewSubject" wire:target="saveNewSubject" wire:loading.class="am-btn_disable">{{ __('general.save_update') }} </button>
                                 </div>
                             </fieldset>
                         </form>
+
+
+
+
                     </div>
                 </div>
             </div>
         </div>
+
+
+
+
+
 
        {{-- final modal   --}}
 
