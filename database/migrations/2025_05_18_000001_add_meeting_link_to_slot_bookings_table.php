5b18bb4bf381f8a9e59caedbd788dd5bb5c7b8e3
alejandoCalzadilla <calzadillanogalesalejandro@gmail.com.com>
2025-07-08 16:37:46 -0400

    migracion

--- database/migrations/2025_05_18_000001_add_meeting_link_to_slot_bookings_table.php
+++ database/migrations/2025_05_18_000001_add_meeting_link_to_slot_bookings_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::table('slot_bookings', function (Blueprint $table) {
             $table->string('meeting_link')->nullable()->after('calendar_event_id');
-            $table->unsignedBigInteger('user_subject_slot_id')->after('tutor_id');
-            $table->foreign('user_subject_slot_id')->references('id')->on('user_subject_slots')->onDelete('cascade');
+           /*  $table->unsignedBigInteger('user_subject_slot_id')->after('tutor_id'); */
+           /*  $table->foreign('user_subject_slot_id')->references('id')->on('user_subject_slots')->onDelete('cascade'); */
         });
     }
 
@@ -24,8 +24,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('slot_bookings', function (Blueprint $table) {
-            $table->dropForeign(['user_subject_slot_id']);
-            $table->dropColumn('user_subject_slot_id');
+            /* $table->dropForeign(['user_subject_slot_id']); */
+            /* $table->dropColumn('user_subject_slot_id'); */
             $table->dropColumn('meeting_link');
         });
     }
