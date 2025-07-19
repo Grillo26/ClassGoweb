dfa5fb35bb13c66413b5dfbd203e2e86e755fdae
Alvaro Rojas <rojasmachucaalvaro@gmail.com>
2025-07-19 18:14:05 -0400

    volviendo para atras

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+// Autorización para canales privados de usuario
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
